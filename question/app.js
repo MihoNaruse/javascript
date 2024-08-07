1707049c26932b6219324b6c6ac7fa9199ed3c02
miho naruse <kurageroge133@gmail.com>
2024-08-07 19:34:04 +0900

    全部の問題に番号をつけた

--- question/app.js
+++ question/app.js
@@ -1,17 +1,17 @@
-//Q1
+// Q1
 let nickname = 'ごっしー'
 let age = 28
 let greet = '私のニックネームは'+ nickname + 'です。年齢は' + age + '歳です。'
 console.log(greet);
 
-//Q2
+// Q2
 let languages = ["JavaScript", "PHP", "Ruby", "Python", "Go"];
 let favoriteLanguage = languages[0]; // JavaScript
 let nextLanguage = languages[3]; // Python
 
 console.log(`私の好きな言語は${favoriteLanguage}です。次は${nextLanguage}を勉強してみたいです。`);
 
-//Q3
+// Q3
 let user = {
   name: 'John',
   age: 26,
@@ -20,7 +20,7 @@ let user = {
 };
 console.log(user.age);
 
-
+// Q04
 let playerList = [
   {
     name: 'John',
@@ -40,6 +40,7 @@ let playerList = [
 ];
 console.log(playerList[1].favorites[1])
 
+// Q5
 let totalAge = playerList[0].age + playerList[1].age + playerList[2].age;
 let averageAge = totalAge / playerList.length;
 console.log(averageAge);
@@ -49,11 +50,13 @@ function sayHello() {
 }
 sayHello();
 
+// Q6
 let sayWorld = function() {
   console.log("World");
 };
 sayWorld();
 
+// Q7
 let updatedUser = {
   ...user,
   birthday: '2000-09-27'
@@ -70,6 +73,7 @@ updatedUser = {
 
 updatedUser.sayHello();
 
+// Q8
 let calc = {};
 
 calc.add = function(x, y) {
@@ -92,21 +96,26 @@ calc.divide = function(x,y) {
 };
 calc.divide(15,3);
 
+// Q9
 function remainder(x, y) {
   return x % y;
 }
 let result = remainder(5, 3);
 console.log(`5 を 3 で割った余りは ${result} です。`);
 
+// Q10
 //変数 x は foo 関数のスコープ内で宣言されているため、関数外から参照することはできない。
 
+//応用Q1
 let randomInteger = Math.floor(Math.random() * 10);
 console.log(randomInteger);
 
+//応用Q2
 setTimeout(function() {
   console.log("Hello World!");
 }, 3000);
 
+//応用Q3
 let num = 5;
 if (num > 0) {
   console.log("num is greater than 0");
@@ -116,12 +125,14 @@ if (num > 0) {
   console.log("num is 0");
 }
 
+//応用Q4
 let numbers = [];
 for (let i = 0; i < 100; i++) {
   numbers.push(i);
 }
 console.log(numbers);
 
+//応用Q5
 let mixed = [4, '2', 5, '8', '9', 0, 1];
 for (let i = 0; i < mixed.length; i++) {
   let item = mixed[i];
